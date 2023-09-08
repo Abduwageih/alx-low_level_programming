ce471a28814b23fe6aa077bd19320e045a9b656f
abduwageih <abduwageih@gmail.com>
2023-09-08 10:43:44 -0700

    55

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include	<stdio.h>
+#include <stdio.h>
 /**
  * Description: main - Prints out a string to stdout.
  * Return: 0 if success.
@@ -7,3 +7,4 @@ int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
+}
