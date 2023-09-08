1eea604014fd229e8a49ff3852751ebb49c61b4d
abduwageih <abduwageih@gmail.com>
2023-09-08 10:10:23 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#!/bin/bash
-#include <stdio.h>
+
+#include	<stdio.h>
 
 /**
- * main - this is the main put function
- * Return: 0
+ * main	-	this is a main function
+ * Return:	always	0
  */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+int	main(void)
+	{
+		puts	("\"Programming is like building a multilingual puzzle")
+			return	(0);
+	}
