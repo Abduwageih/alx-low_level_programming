ddc714c82e8745f676dd1a977f3c179ecbede82c
abduwageih <abduwageih@gmail.com>
2023-09-14 16:10:52 -0700

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests function that print if int is pos or neg
+ * return: 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
